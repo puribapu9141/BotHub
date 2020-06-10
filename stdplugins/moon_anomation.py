79f043d314217521f4ae6e84f0293ad56fe8c7d9
Pratik Goswami PM <54017670+puribapu9141@users.noreply.github.com>
2020-06-10 19:33:01 +0530

    Create moon_anomation.py

---
+++ stdplugins/moon_anomation.py
@@ -0,0 +1,17 @@
+# (c) @UniBorg
+""" command: .moon """
+from telethon import events
+import asyncio
+from collections import deque
+
+
+@borg.on(events.NewMessage(pattern=r"\.moon", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("ðŸŒ—ðŸŒ˜ðŸŒ‘ðŸŒ’ðŸŒ“ðŸŒ”ðŸŒ•ðŸŒ–"))
+	for _ in range(32):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+    
